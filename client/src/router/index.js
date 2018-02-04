1d50f8d0f14ccc96913a2c7ca9c5d5a02cb63bb8
Artem A <artem.v.andreev@gmail.com>
2018-02-04 19:21:01 +0200

    add lazy loding components

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+const lazyLoad = (component) => import(`@/components/${component}`)
+
 export default new Router({
+  mode: 'history',
+
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: () => lazyLoad('Home')
     }
   ]
 })

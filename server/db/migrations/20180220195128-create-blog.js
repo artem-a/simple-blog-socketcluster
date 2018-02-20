40412eaa7b199c5ed268e8e634d2c8aaadd9b325
Artem A <artem.v.andreev@gmail.com>
2018-02-20 22:38:38 +0200

    fix eslint issue, add Blog model

---
+++ server/db/migrations/20180220195128-create-blog.js
@@ -0,0 +1,49 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('blogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'cascade',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      slug: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+    .then(() => queryInterface
+      .addIndex('blogs', { fields: ['slug'], unique: true })
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Blogs')
+  }
+}
